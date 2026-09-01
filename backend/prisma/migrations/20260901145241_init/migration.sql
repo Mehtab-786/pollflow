a69ea8252ed9df22ba09f5f57bf4cd9958f52274
Mehtab Hussain <tradercoder786@gmail.com>
2026-09-02 00:06:31 +0530

    Prisma setup

---
+++ backend/prisma/migrations/20260901145241_init/migration.sql
@@ -0,0 +1,138 @@
+-- CreateEnum
+CREATE TYPE "ResponseMode" AS ENUM ('AUTHENTICATED', 'ANONYMOUS');
+
+-- CreateEnum
+CREATE TYPE "PollType" AS ENUM ('POLL', 'QUIZ');
+
+-- CreateEnum
+CREATE TYPE "QuestionType" AS ENUM ('TEXT', 'MULTIPLE_CHOICE');
+
+-- CreateEnum
+CREATE TYPE "SelectionMode" AS ENUM ('SINGLE', 'MULTIPLE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "tokenHash" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Poll" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "creatorId" TEXT NOT NULL,
+    "responseMode" "ResponseMode" NOT NULL,
+    "expiresAt" TIMESTAMP(3),
+    "isPublished" BOOLEAN NOT NULL DEFAULT false,
+    "type" "PollType" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Poll_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Question" (
+    "id" TEXT NOT NULL,
+    "question" TEXT NOT NULL,
+    "position" INTEGER NOT NULL,
+    "pollId" TEXT NOT NULL,
+    "isRequired" BOOLEAN NOT NULL,
+    "type" "QuestionType" NOT NULL,
+    "selectionMode" "SelectionMode" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Question_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Option" (
+    "id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "questionId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Option_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Response" (
+    "id" TEXT NOT NULL,
+    "respondentId" TEXT,
+    "pollId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Response_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ResponseAnswer" (
+    "id" TEXT NOT NULL,
+    "responseId" TEXT NOT NULL,
+    "questionId" TEXT NOT NULL,
+    "optionId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ResponseAnswer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "Poll_creatorId_idx" ON "Poll"("creatorId");
+
+-- CreateIndex
+CREATE INDEX "Question_pollId_idx" ON "Question"("pollId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Question_pollId_position_key" ON "Question"("pollId", "position");
+
+-- CreateIndex
+CREATE INDEX "Option_questionId_idx" ON "Option"("questionId");
+
+-- CreateIndex
+CREATE INDEX "Response_pollId_idx" ON "Response"("pollId");
+
+-- CreateIndex
+CREATE INDEX "Response_respondentId_idx" ON "Response"("respondentId");
+
+-- CreateIndex
+CREATE INDEX "ResponseAnswer_responseId_idx" ON "ResponseAnswer"("responseId");
+
+-- CreateIndex
+CREATE INDEX "ResponseAnswer_questionId_idx" ON "ResponseAnswer"("questionId");
+
+-- CreateIndex
+CREATE INDEX "ResponseAnswer_optionId_idx" ON "ResponseAnswer"("optionId");
+
+-- AddForeignKey
+ALTER TABLE "Poll" ADD CONSTRAINT "Poll_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Question" ADD CONSTRAINT "Question_pollId_fkey" FOREIGN KEY ("pollId") REFERENCES "Poll"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Option" ADD CONSTRAINT "Option_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Response" ADD CONSTRAINT "Response_respondentId_fkey" FOREIGN KEY ("respondentId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Response" ADD CONSTRAINT "Response_pollId_fkey" FOREIGN KEY ("pollId") REFERENCES "Poll"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ResponseAnswer" ADD CONSTRAINT "ResponseAnswer_responseId_fkey" FOREIGN KEY ("responseId") REFERENCES "Response"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ResponseAnswer" ADD CONSTRAINT "ResponseAnswer_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ResponseAnswer" ADD CONSTRAINT "ResponseAnswer_optionId_fkey" FOREIGN KEY ("optionId") REFERENCES "Option"("id") ON DELETE SET NULL ON UPDATE CASCADE;
---
+++ backend/src/common/config/db.config.ts
@@ -0,0 +1,12 @@
+// src/common/config/db.config.ts
+import "dotenv/config";
+import { PrismaClient } from "../../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
